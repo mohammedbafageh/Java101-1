acd45068d42af437f165f6644cc38ec6be0e15bf
MrHani1989 <91114559+MrHani1989@users.noreply.github.com>
2021-09-21 09:46:01 +0300

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,3 +1,20 @@
+/*
+Ask the user to enter two float digits. For example ( 3.5 and 11.25 )
+
+Print the calculation for the two digits in separate lines:
+
+Addition +
+Subtraction  -
+Multiplication  *
+Division /
+Modulus %
+Grater >
+Grater or equal
+Less 
+Less or equal
+Not equal
+Equal
+*/
 
 //Your Name
 //Your ID
