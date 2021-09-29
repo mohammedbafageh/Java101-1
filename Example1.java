228912665a52284c4e52d03136c14fc119e087b7
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-09-29 19:25:11 +0300

    Create Example1.java

---
+++ Example1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        /*
+        ( 90 , 100 ) print A
+        ( 80 , 90 ] print B
+        ( 70 , 80 ] print c
+        ( 60 , 70 ] print d
+        ( 0 , 60 ] print f
+         */
+    }
+}
