87eddc1547e595096c81afff845d29fc6c6c01b1
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-09-30 11:52:42 +0300

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Assignment1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        /*
+        Ask the user to enter a number 
+        check if the rest of division by 2 is equal to zero then print Even
+        else print Odd
+         */
+    }
+}
