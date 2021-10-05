4b85a7728c660cbb9d50442de9f8494c428885b9
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-10-05 08:13:38 +0300

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Assignment1 {
+public class EvenOrOdd {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         /*
