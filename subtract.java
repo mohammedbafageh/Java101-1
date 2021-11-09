b7c144451d002d9d2bf2ae0a8f5d8028bc008512
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-11-09 09:39:17 +0300

    Create subtract.java

---
+++ subtract.java
@@ -0,0 +1,10 @@
+public class Subtract {
+
+    static float subtractFunction(float x,int y){
+        return x-y;
+    }
+
+    public static void main(String[] args){
+        System.out.println(subtractFunction(5.7f,26));
+    }
+}
