125fd6c62aa49cbde2f0c8ffaa8ceb66eea10870
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-11-09 09:35:42 +0300

    Create powFunction.java

---
+++ powFunction.java
@@ -0,0 +1,10 @@
+public class powFunction {
+
+    static float pow(float num){
+        return num * num;
+    }
+
+    public static void main(String[] args){
+        System.out.println(pow(2.5f));
+    }
+}
