89cd23e7c9244666cbfa31b7c21ce0095b49f2d4
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2021-11-18 11:22:55 +0300

    Create MethodsExc.java

---
+++ MethodsExc.java
@@ -0,0 +1,9 @@
+
+public class MethodsExc {
+    public static void main(String[] args){
+        System.out.print(add(1f,2f,4f));
+        System.out.print(sub(4f,3f,2f));
+        System.out.print(mult(5f,10f));
+        System.out.print(div(5f,4f));
+    }
+}
