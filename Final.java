d1504e19298f3dc5d474e337e7166fe467adcb7d
Hani M. Th <91114559+MrHani1989@users.noreply.github.com>
2022-06-01 19:59:37 +0300

    Create Final.java

---
+++ Final.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    //قم بالتعديل على الدالة بحيث تمرر لها متغيرين من نوع عدد حقيقي
+    public static float add() {
+        // قم بجمع المتغيرين و إعادة النتيجة
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        // اطلب من المستخدم أن يدخل العدد الأول
+        float x = scan.nextFloat();
+        System.out.println("Enter Second number");
+        // قم بتخزين المتغير الثاني
+
+
+        //اطبع نتيجة استدعاء دالة الجمع
+    }
+}
